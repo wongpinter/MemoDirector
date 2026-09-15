9914d4cbd54a86f0e5b7d238b029f549e21f6bbf
wongpinter <the.wongpinter@gmail.com>
2026-09-15 10:29:25 +0700

    chore: enable allowedHosts for Tailscale access in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     host: '0.0.0.0',
-    allowedHosts: ['memo.dev.wongpinter.com'],
+    allowedHosts: true,
   },
   plugins: [react()],
   resolve: {
